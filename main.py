06c38474ffd0f64f6ffd149961b2a70d8bde94e5
louie-jones-strong <33354741+louie-jones-strong@users.noreply.github.com>
2018-05-29 21:00:11 +0100

    update

--- main.py
+++ main.py
@@ -20,10 +20,11 @@ class face(object):
 		self.centre = (x,y,z)
 
 class object3D(object):
-	def __init__(self, faces, postion3D=[0,0,0], rotation3D=[0,0,0], hidden=False, selected=True):
+	def __init__(self, faces, postion3D=[0,0,0], rotation3D=[0,0,0], hidden=False, selected=False):
 		self.postion3D = postion3D
 		self.rotation3D = rotation3D
 		self.hidden = hidden
+		self.selected = selected
 		self.faces = faces
 		
 class game(object):
@@ -93,7 +94,7 @@ class game(object):
 
 					distance = math.sqrt( centre[0]**2 + centre[1]**2 + (centre[2]-self.FOV)**2 )
 					
-					face_list += [[ distance , face , object3D.faces[loop].colour ]]
+					face_list += [[ distance , face , object3D.faces[loop].colour , objectNum , object3D.selected ]]
 
 		return sorted(face_list)
 
@@ -110,7 +111,10 @@ class game(object):
 			if self.render_mode[2]:
 				pygame.draw.polygon(self.window, face_list[loop][2], face_list[loop][1], 0)	
 			if self.render_mode[1]:
-				pygame.draw.polygon(self.window, [100,100,100], face_list[loop][1], 4)	
+				if face_list[loop][4]:
+					pygame.draw.polygon(self.window, [255,0,0], face_list[loop][1], 4)	
+				else:
+					pygame.draw.polygon(self.window, [100,100,100], face_list[loop][1], 4)	
 
 		if FPS != 0:
 			label = self.Font.render("FPS:"+str(FPS), 1,(255,255,255))
@@ -141,25 +145,62 @@ class game(object):
 		display.init()
 		pygame.font.init()
 		self.resolution = (800,800)
+		self.windowPostion = [0,0]
 		self.globalRotate = [0,0,0]
 		self.FOV = 256
 		self.viewer_distance = 3
 		self.rotation_multiplier = 2
 		self.render_mode = [False,True,True]
 		self.Font = pygame.font.SysFont("monospace", 15)
-		self.window = display.set_mode(self.resolution)
+		if False:
+			self.resolution = (1920,1080)
+			self.window = display.set_mode(self.resolution,pygame.FULLSCREEN )
+		else:
+			self.window = display.set_mode(self.resolution)
 
 		object3D_list = []
 		object3D_list += [object3D(shapes(),postion3D=[-0.5,-0.5,-0.5])]
+		object3D_list += [object3D(shapes(),postion3D=[0.5,-0.5,-0.5])]
 
-		object3D_list[0].faces[0].colour = [255,0,0]
-		object3D_list[0].faces[1].colour = [0,255,0]
-		object3D_list[0].faces[2].colour = [0,0,255]
-		object3D_list[0].faces[3].colour = [255,255,0]
-		object3D_list[0].faces[4].colour = [0,255,255]
-		object3D_list[0].faces[5].colour = [255,0,255]
+		for loop in range(len(object3D_list)):
+			object3D_list[loop].faces[0].colour = [255,0,0]
+			object3D_list[loop].faces[1].colour = [0,255,0]
+			object3D_list[loop].faces[2].colour = [0,0,255]
+			object3D_list[loop].faces[3].colour = [255,255,0]
+			object3D_list[loop].faces[4].colour = [0,255,255]
+			object3D_list[loop].faces[5].colour = [255,0,255]
+
+		temp = mouse.get_position()
+		pygame.mouse.set_pos([0,0])
+		self.windowPostion = mouse.get_position()
+		mouse.move(temp[0], temp[1])
 
 		return object3D_list
+
+	def inside_polygon(self,x, y, points):
+		n = len(points)
+		inside = False
+		p1x, p1y = points[0]
+		for i in range(1, n + 1):
+			p2x, p2y = points[i % n]
+			if y > min(p1y, p2y):
+				if y <= max(p1y, p2y):
+					if x <= max(p1x, p2x):
+						if p1y != p2y:
+							xinters = (y - p1y) * (p2x - p1x) / (p2y - p1y) + p1x
+						if p1x == p2x or x <= xinters:
+							inside = not inside
+			p1x, p1y = p2x, p2y
+		return inside
+
+	def getObjectClicked(self,object3D_list,globalRotate):
+		pos = mouse.get_position()
+		pos = [pos[0]-self.windowPostion[0],pos[1]-self.windowPostion[1]]
+		face2DList = self.objects_to_2DFaceList(object3D_list,globalRotate)
+		for loop in range(len(face2DList)):
+			if self.inside_polygon(pos[0],pos[1],face2DList[loop][1]):
+				return object3D_list[face2DList[loop][3]]
+		return None
 
 	def main(self):
 		object3D_list = self.setup()
@@ -169,12 +210,11 @@ class game(object):
 
 		while True:
 			time_taken = time.time()
-			globalRotate = [self.globalRotate[0],self.globalRotate[1],self.globalRotate[2]]
-			if mouse.is_pressed(button='right') and not draging:
+			if mouse.is_pressed(button='middle') and not draging:
 				startPos = mouse.get_position()
 				draging = True
 
-			elif not mouse.is_pressed(button='right') and draging:
+			elif not mouse.is_pressed(button='middle') and draging:
 				change = [0,0]
 				finishPos = mouse.get_position()
 				change[0] = (startPos[1]-finishPos[1])/self.resolution[1]
@@ -183,13 +223,25 @@ class game(object):
 				self.globalRotate[1] = (self.globalRotate[1] + 360*change[1])%360 
 				draging = False
 
-			elif mouse.is_pressed(button='right') and draging:
+			globalRotate = [self.globalRotate[0],self.globalRotate[1],self.globalRotate[2]]
+			if mouse.is_pressed(button='middle') and draging:
 				change = [0,0]
 				finishPos = mouse.get_position()
 				change[0] = (startPos[1]-finishPos[1])/self.resolution[1]
 				change[1] = (startPos[0]-finishPos[0])/self.resolution[0]
 				globalRotate[0] = (globalRotate[0] + 360*change[0])%360
 				globalRotate[1] = (globalRotate[1] + 360*change[1])%360 
+
+
+			if mouse.is_pressed(button='right'):
+				object3D = self.getObjectClicked(object3D_list,globalRotate)
+				if not object3D == None:
+					time.sleep(0.1)
+					if object3D.selected:
+						object3D.selected = False
+					else:
+						object3D.selected = True
+
 
 			self.update_window(object3D_list,globalRotate,FPS=FPS)
 
@@ -200,8 +252,8 @@ class game(object):
 
 
 			time_taken = time.time() - time_taken
-			if time_taken < 1/65:
-				time_taken = 1/65-time_taken
+			if time_taken < 1/60:
+				time_taken = 1/60-time_taken
 				time.sleep(time_taken)
 
 			if FPS_count >= 30:
