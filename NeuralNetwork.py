7d6e79a9031c981ede762f192526d4a79a866ef4
Louie <louiej-s@hotmail.co.uk>
2019-02-02 22:47:55 +0000

    showing how many boards have been loaded

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -18,17 +18,24 @@ class NeuralNetwork(object):
 		dataSet = pickle.load(file)
 		file.close()
 
+		loop = 0
 		for key, value in dataSet.items():
 			self.DataSetX += [key]
 
 			temp = []
-			for loop in range(4096):#len of move to move ids list
-				if loop in value.Moves:
+			for loop2 in range(4096):#len of move to move ids list
+				if loop2 in value.Moves:
 					temp += [1]
 				else:
 					temp += [0]
 
 			self.DataSetY += [temp]
+			
+			if loop % 500 == 0:
+				temp = int((loop/len(dataSet))*100)
+				os.system("cls")
+				print(str(temp)+"% "+str(loop)+"/"+str(len(dataSet)))
+			loop += 1
 
 		return 
 
