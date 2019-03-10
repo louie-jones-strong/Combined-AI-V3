e238b5f057234e9fb8b1bc849946c5d19edd34c6
louie-jones-strong <louiej-s@hotmail.co.uk>
2019-03-10 00:51:19 +0000

    in progress

--- BruteForce.py
+++ BruteForce.py
@@ -3,73 +3,9 @@ import os
 import sys
 import random
 
-class DataSetLoadAndSaver(object):
-	def __init__(self, datasetAddress):
-		self.HashTableAddress = datasetAddress+"DataSetHashTable.p"
-		self.TableAddress = datasetAddress
-		self.batchSize = 20000
-		self.DataBaseHashTable = {}
-		self.DataSetTables = []
-		return
-
-	def SaveBruteForceDataSet(self, dataset):
-		fillingTable = 0
-		for loop in range(len(self.DataSetTables)):
-			if len(self.DataSetTables[loop]) >= self.batchSize:
-				fillingTable += 1
-
-		tablesToSave = []
-		for key, value in dataset.items():
-
-			if key in self.DataBaseHashTable:
-				index = self.DataBaseHashTable[key]
-
-			else:
-				self.DataBaseHashTable[key] = fillingTable
-				index = fillingTable
-				if len(self.DataSetTables) <= index:
-					self.DataSetTables += [{}]
-
-				if len(self.DataSetTables[index]) >= self.batchSize:
-					fillingTable += 1
-			
-			self.DataSetTables[index][key] = value
-			if index not in tablesToSave:
-				tablesToSave += [index]
-
-		for loop in range(len(tablesToSave)):
-			index = tablesToSave[loop]
-			pickle.dump(self.DataSetTables[index], open(self.TableAddress+"Table_"+str(index)+".p", "wb"))
-
-		pickle.dump(self.DataBaseHashTable, open(self.HashTableAddress, "wb"))
-		return
-
-	def LoadBruteForceDataSet(self):
-		file = open(self.HashTableAddress, "rb")
-		self.DataBaseHashTable = pickle.load(file)
-		file.close()
-		
-		numberOfTables = 0
-		for file in os.listdir(self.TableAddress):
-			if file.startswith("Table_") and file.endswith(".p"):
-				numberOfTables += 1
-
-
-		output = {}
-		self.DataSetTables = []
-		for loop in range(numberOfTables):
-			file = open(self.TableAddress+"Table_"+str(loop)+".p", "rb")
-			self.DataSetTables += [pickle.load(file)]
-			file.close()
-			output.update(self.DataSetTables[loop])
-		
-		return output
-
 class DataSetManager(object):
-	DataSet = {}
 	NumberOfCompleteBoards = 0
 	MoveIDLookUp = []
-	BoardToHashLookUp = {}
 	MaxMoveIDs = 0
 	BoardsToUpdate = {}
 	
@@ -81,12 +17,18 @@ class DataSetManager(object):
 		self.OutputResolution = outputResolution
 
 		self.MaxMoveIDs = int(((maxOutputSize-(minOutputSize-1))*(1/outputResolution) )**numOfOutputs)
-		self.DatasetAddress = datasetAddress+"BruteForceDataSet//"
+		self.HashTableAddress = datasetAddress+"BruteForceDataSet//DataSetHashTable.p"
+		self.TableAddress = datasetAddress+"BruteForceDataSet//"
 		self.BoardHashLookUpAddress = datasetAddress+"LookUp//"+"BoardHashLookup"
 		self.MoveIDLookUpAdress = datasetAddress+"LookUp//"+"MoveIdLookUp"
+		self.TableBatchSize = 20000
 
-		if not os.path.exists(datasetAddress+"BruteForceDataSet//"):
-			os.makedirs(datasetAddress+"BruteForceDataSet//")
+		self.DataBaseHashTable = {}
+		self.DataSetTables = []
+		self.BoardToHashLookUp = {}
+
+		if not os.path.exists(self.TableAddress):
+			os.makedirs(self.TableAddress)
 		if not os.path.exists(datasetAddress+"LookUp//"):
 			os.makedirs(datasetAddress+"LookUp//")
 
@@ -94,38 +36,87 @@ class DataSetManager(object):
 		self.MoveIDLookUp = []
 		for loop in range(self.MaxMoveIDs):
 			self.MoveIDLookUp += [self.MoveIDToMove(loop)]
-			
-		self.LoadAndSaver = DataSetLoadAndSaver(self.DatasetAddress)
 		return
 
-	def SaveDataSet(self, forceSave=False):
+	def SaveDataSet(self):
 		if (not os.path.exists(self.MoveIDLookUpAdress + ".p")):
 			pickle.dump(self.MoveIDLookUp, open(self.MoveIDLookUpAdress + ".p", "wb"))
 
 		pickle.dump(self.BoardToHashLookUp, open(self.BoardHashLookUpAddress + ".p", "wb"))
-
-		if forceSave:
-			self.LoadAndSaver.SaveBruteForceDataSet(self.DataSet)
-		else:
-			boardsToSave = {}
-			for key in self.BoardsToUpdate:
-				boardsToSave[key] = self.DataSet[key]
-
-			self.LoadAndSaver.SaveBruteForceDataSet(boardsToSave)
+		boardsToSave = {}
+		for key in self.BoardsToUpdate:
+			boardsToSave[key] = self.DataSet[key]
 
 		self.BoardsToUpdate = {}
+
+		fillingTable = 0
+		for loop in range(len(self.DataSetTables)):
+			if len(self.DataSetTables[loop]) >= self.TableBatchSize:
+				fillingTable += 1
+
+		tablesToSave = []
+		for key, value in boardsToSave.items():
+
+			if key in self.DataBaseHashTable:
+				index = self.DataBaseHashTable[key]
+
+			else:
+				self.DataBaseHashTable[key] = fillingTable
+				index = fillingTable
+				if len(self.DataSetTables) <= index:
+					self.DataSetTables += [{}]
+
+				if len(self.DataSetTables[index]) >= self.TableBatchSize:
+					fillingTable += 1
+			
+			self.DataSetTables[index][key] = value
+			if index not in tablesToSave:
+				tablesToSave += [index]
+
+		for loop in range(len(tablesToSave)):
+			index = tablesToSave[loop]
+			pickle.dump(self.DataSetTables[index], open(self.TableAddress+"Table_"+str(index)+".p", "wb"))
+
+		pickle.dump(self.DataBaseHashTable, open(self.HashTableAddress, "wb"))
+
 		return
-	
 	def LoadDataSet(self):
 		if not os.path.isfile(self.BoardHashLookUpAddress + ".p"):
 			return False
-
-		self.DataSet = self.LoadAndSaver.LoadBruteForceDataSet()
-
 		file = open(self.BoardHashLookUpAddress + ".p", "rb")
 		self.BoardToHashLookUp = pickle.load(file)
 		file.close()
+
+		file = open(self.HashTableAddress, "rb")
+		self.DataBaseHashTable = pickle.load(file)
+		file.close()
+		
+		self.DataSetTables = []
+		numberOfTables = 0
+		for file in os.listdir(self.TableAddress):
+			if file.startswith("Table_") and file.endswith(".p"):
+
+				file = open(self.TableAddress+"Table_"+str(numberOfTables)+".p", "rb")
+				self.DataSetTables += [pickle.load(file)]
+				file.close()
+				
+				numberOfTables += 1
 		return True
+	
+	def GetBoardInfo(self, key):
+		boardInfo = None
+		found = False
+
+		if key in self.DataBaseHashTable:
+			index = self.DataBaseHashTable[key]
+			boardInfo = self.DataSetTables[index][key]
+			found = True
+		
+		return found, boardInfo
+
+	def AddNewBoard(self , key):
+
+		return
 
 	def MoveIDToMove(self, moveID):
 		temp = int((self.MaxOutputSize-(self.MinOutputSize-1))*(1/self.OutputResolution))
@@ -136,8 +127,7 @@ class DataSetManager(object):
 		return move
 
 	def BoardToKey(self, board):
-		key = str(board)
-		key = key.replace(" ", "")
+		key = hash(str(board))
 
 		if not key in self.BoardToHashLookUp:
 			self.BoardToHashLookUp[key] = board
@@ -155,45 +145,45 @@ class BruteForce(object):
 
 	def MoveCal(self, board):
 		key = self.DataSetManager.BoardToKey(board)
+		found, boardInfo = self.DataSetManager.GetBoardInfo(key)
 
 		if self.WinningModeON:
 			print("winnning mode!")
-			if key in self.DataSetManager.DataSet and len(self.DataSetManager.DataSet[key].Moves) > 0:
-				moveID = self.DataSetManager.DataSet[key].MoveIDOfBestAvgFitness
+			if key in self.DataSetManager.DataSet and len(boardInfo.Moves) > 0:
+				moveID = boardInfo.MoveIDOfBestAvgFitness
 
 			else:#never played board before
 				moveID = random.randint(0,self.DataSetManager.MaxMoveIDs-1)
 				print("new Board!")
 
 		else:  # learning mode
-			if key in self.DataSetManager.DataSet:
+			if found:
 				
-				if self.DataSetManager.DataSet[key].NumOfTriedMoves > self.DataSetManager.MaxMoveIDs:
-					if len(self.DataSetManager.DataSet[key].Moves) == 0:
+				if boardInfo.NumOfTriedMoves > self.DataSetManager.MaxMoveIDs:
+					if len(boardInfo.Moves) == 0:
 						input("error!!!")
 
-				if self.DataSetManager.DataSet[key].NumOfTriedMoves < self.DataSetManager.MaxMoveIDs:
-					moveID = self.DataSetManager.DataSet[key].NumOfTriedMoves
-					self.DataSetManager.DataSet[key].Moves[moveID] = MoveInfo(MoveID=moveID)
-					self.DataSetManager.DataSet[key].NumOfTriedMoves += 1
+				if boardInfo.NumOfTriedMoves < self.DataSetManager.MaxMoveIDs:
+					moveID = boardInfo.NumOfTriedMoves
+					boardInfo.Moves[moveID] = MoveInfo(MoveID=moveID)
+					boardInfo.NumOfTriedMoves += 1
 
-					if self.DataSetManager.DataSet[key].NumOfTriedMoves >= self.DataSetManager.MaxMoveIDs:
+					if boardInfo.NumOfTriedMoves >= self.DataSetManager.MaxMoveIDs:
 						self.DataSetManager.NumberOfCompleteBoards += 1
 
 				else:#played every board once already
 					leastPlayed = sys.maxsize
 					moveID = 0
-					for movekey, moveValue in self.DataSetManager.DataSet[key].Moves.items():
+					for movekey, moveValue in boardInfo.Moves.items():
 						if moveValue.TimesPlayed < leastPlayed:
 							leastPlayed = moveValue.TimesPlayed
 							moveID = movekey
 					
-					self.DataSetManager.DataSet[key].Moves[moveID].TimesPlayed += 1
+					boardInfo.Moves[moveID].TimesPlayed += 1
 
 			else:#never played board before
 				moveID = 0
-				moveInfo = {moveID : MoveInfo(MoveID=moveID)}
-				self.DataSetManager.DataSet[key] = BoardInfo(Moves=moveInfo)
+				self.DataSetManager.AddNewBoard(key)
 
 			#cahnge tempdataset to dict so faster to del for bigger lists
 			self.TempDataSet[str(key)+str(moveID)] = {"BoardKey": key, "MoveID": moveID}
@@ -264,7 +254,6 @@ class MoveInfo():
 		self.TimesPlayed = TimesPlayed
 		self.MoveID = MoveID
 		return
-
 
 # 1) point to the move that is least played
 # 2) make datasetmanager and datasetloadandsaver in to one to save ram 
