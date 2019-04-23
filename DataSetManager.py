991e2b35090dee8afb6530c2637861cf8ea24720
Louie <louiej-s@hotmail.co.uk>
2019-04-23 08:24:40 +0100

    table number in loadings

--- DataSetManager.py
+++ DataSetManager.py
@@ -206,7 +206,7 @@ class DataSetManager(object):
 			if len(self.DataSetTables[index].Content) < self.TableBatchSize and self.FillingTable == -1:
 				self.FillingTable = index
 			self.DataSetTables[index].Unload()
-			LoadingBar(((loop/numberOfTables)/9)*10, "loading tables...")
+			LoadingBar(((loop/numberOfTables)/9)*10, "loading table: "+loop+"/"+numberOfTables)
 
 			index += 1
 			
