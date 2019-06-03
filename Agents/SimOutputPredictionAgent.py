e484248fdf83feeb47ecfbfc81f9ea1805b732fe
louie-jones-strong <louiej-s@hotmail.co.uk>
2019-06-03 23:43:54 +0100

    first pass on board predictor

--- Agents/SimOutputPredictionAgent.py
+++
@@ -1,6 +0,0 @@
-
-class SimOutputPrediction:
-
-	def __init__(self):
-
-		return
---
+++ Agents/SimOutputPredictor.py
@@ -0,0 +1,31 @@
+
+class SimOutputPredictor:
+
+	def __init__(self, dataSetManager, loadData):
+		self.DataSetManager = dataSetManager
+		self.TempDataSet = {}
+
+		if loadData:
+			self.DataSetManager.LoadTableInfo()
+		return
+
+	def PredictOutput(self, board, move):
+		newBoard = board[:]
+
+		key = self.DataSetManager.BoardToKey(board)
+		found, boardInfo = self.DataSetManager.GetBoardInfo(key)
+
+		if found:
+			moveID = self.DataSetManager.MoveIDLookUp.index(move)
+			moveOutComes = boardInfo[moveID].MoveOutComes
+			
+			highestTimes = 0
+			for outCome, times in moveOutComes.items():
+						
+				if highestTimes < times:
+					newBoard = outCome 
+
+		else:
+			print("need to code ann for PredictOutput")
+
+		return newBoard
