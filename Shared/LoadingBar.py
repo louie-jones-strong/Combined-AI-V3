eeb78e9602ff6f00edb3e692cae41404c22f0396
louie-jones-strong <louiej-s@hotmail.co.uk>
2019-06-07 00:19:55 +0100

    fix for loading bar

--- Shared/LoadingBar.py
+++ Shared/LoadingBar.py
@@ -6,35 +6,38 @@ class LoadingBar():
 
 	def __init__(self, allowUpdate=True):
 		self.CurrentProgress = 0
-		self.CurrentText = ""
+		self.CurrentText = None
 		self.Resolution = 100
 		self.LastUpdateTime = 0
 		self.AllowUpdate = allowUpdate
 		return
 
-	def Update(self, progress, numDone=None, totalNum=None, text=""):
+	def Update(self, progress, text=None, numDone=None, totalNum=None):
 		progress = max(progress, 0)
 		progress = min(progress, 1)
 
 		progress = int(progress*self.Resolution)
 
 		if time.time()-self.LastUpdateTime >= 0.15 or progress == self.Resolution:
+
 			if progress != self.CurrentProgress or text != self.CurrentText:
 				os.system("cls")
 				bar = "#"*progress
 				fill = " "*(self.Resolution-progress)
 				print("|"+bar+fill+"|")
-				print(text)
+				self.CurrentProgress = progress
+
+				if text != None:
+					print(text)
+					self.CurrentText = text
 				
 				if numDone != None and totalNum != None:
 					output = Formating.SplitNumber(numDone)
 					output += "/"
 					output += Formating.SplitNumber(totalNum)
-					output += " "+round(progress, 2)+"%"
+					output += " "+str(round(progress, 2))+"%"
 
 					print(output)
 
-				self.CurrentProgress = progress
-				self.CurrentText = text
 				self.LastUpdateTime = time.time()
 		return
