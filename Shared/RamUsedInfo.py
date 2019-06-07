6c61faba4887263182aff06d99dd3e6155c56a6d
Louie <louiej-s@hotmail.co.uk>
2019-06-07 21:38:39 +0100

    bug fix

--- Shared/RamUsedInfo.py
+++ Shared/RamUsedInfo.py
@@ -4,16 +4,6 @@ import sys
 def GetFullSizeOf(inputObject):
 	size = sys.getsizeof(inputObject)
 	
-	if type(inputObject) is dict:
-		for key, value in inputObject.items():
-			size += GetFullSizeOf(key)
-			size += GetFullSizeOf(value)
-
-	elif (hasattr(inputObject, "__len__") and 
-		type(inputObject) is not str):
-
-		for loop in range(len(inputObject)):
-			size += GetFullSizeOf(inputObject[loop])
 
 	return size
 
