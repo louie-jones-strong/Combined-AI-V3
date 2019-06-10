84d14ed93b825c47848bb217ee85441692f6be8c
louie-jones-strong <louiej-s@hotmail.co.uk>
2019-06-11 00:12:57 +0100

    split how move cal is done for ann

--- Agents/NeuralNetworkAgent.py
+++ Agents/NeuralNetworkAgent.py
@@ -27,61 +27,65 @@ class Agent(AgentBase.AgentBase):
 			self.Train()
 		return
 
-	def MoveCal(self, inputs, batch=False):
+	def MoveCal(self, boards, batch=False):
 		if not batch:
-			inputs = [inputs]
+			boards = [boards]
 
-		networkOutputs = self.NetworkModel.predict(inputs)
+		networkOutputs = self.NetworkModel.predict(boards)
 
 		outputs = []
 		for loop in range(len(networkOutputs)):
 			networkOutput = list(networkOutputs[loop])
 
-			if self.DataSetManager.MetaData["NetworkUsingOneHotEncoding"]:
-				key = self.DataSetManager.BoardToKey(inputs[loop])
-				found, boardInfo = self.DataSetManager.GetBoardInfo(key)
-
-				output = self.DataSetManager.MoveIDToMove(0)
-				bestValue = -sys.maxsize
-
-				for loop2 in range(len(networkOutput)):
-					invaild = False
-
-					if found:
-						if 2**loop2 & boardInfo.PlayedMovesLookUpArray:
-							if loop2 not in boardInfo.Moves:
-								invaild = True
-
-					if networkOutput[loop2] >= bestValue and (not invaild):
-						bestValue = networkOutput[loop2]
-						output = self.DataSetManager.MoveIDToMove(loop2)
-
-			else:
-				output = []
-				for loop2 in range(len(networkOutput)):
-					temp = networkOutput[loop2]
-					temp = temp / self.DataSetManager.OutputResolution
-					temp = round(temp)
-					temp = temp * self.DataSetManager.OutputResolution
-
-					if temp < self.DataSetManager.MinOutputSize:
-						temp = self.DataSetManager.MinOutputSize
-
-					if temp > self.DataSetManager.MaxOutputSize:
-						temp = self.DataSetManager.MaxOutputSize
-
-					if self.DataSetManager.OutputResolution == int(self.DataSetManager.OutputResolution):
-						temp= int(temp)
-
-					output += [temp]
-
-			outputs += [output]
+			outputs += [self.PredictionToMove(networkOutput, boards[loop])]
 
 		if not batch:
 			outputs = outputs[0]
-			self.RecordMove(inputs[0], outputs)
+			self.RecordMove(boards[0], outputs)
 
 		return outputs
+
+	def PredictionToMove(self, networkOutput, board):
+		if self.DataSetManager.MetaData["NetworkUsingOneHotEncoding"]:
+
+			key = self.DataSetManager.BoardToKey(board)
+			found, boardInfo = self.DataSetManager.GetBoardInfo(key)
+
+			output = self.DataSetManager.MoveIDToMove(0)
+			bestValue = -sys.maxsize
+
+			for loop in range(len(networkOutput)):
+				invaild = False
+
+				if found:
+					if 2**loop & boardInfo.PlayedMovesLookUpArray:
+						if loop not in boardInfo.Moves:
+							invaild = True
+
+				if networkOutput[loop] >= bestValue and (not invaild):
+					bestValue = networkOutput[loop]
+					output = self.DataSetManager.MoveIDToMove(loop)
+
+		else:
+			output = []
+			for loop in range(len(networkOutput)):
+				temp = networkOutput[loop]
+				temp = temp / self.DataSetManager.OutputResolution
+				temp = round(temp)
+				temp = temp * self.DataSetManager.OutputResolution
+
+				if temp < self.DataSetManager.MinOutputSize:
+					temp = self.DataSetManager.MinOutputSize
+
+				if temp > self.DataSetManager.MaxOutputSize:
+					temp = self.DataSetManager.MaxOutputSize
+
+				if self.DataSetManager.OutputResolution == int(self.DataSetManager.OutputResolution):
+					temp= int(temp)
+
+				output += [temp]
+
+		return output
 
 	def Train(self):
 		dataSetX = []
