ec20598d40e95e5959ec2c98b0cd9ff2d4711b78
louie-jones-strong <louiej-s@hotmail.co.uk>
2019-06-23 20:34:16 +0100

    update for tree viewer

--- TreeVisualiser.py
+++ TreeVisualiser.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import networkx as nx
+import time
 
 class TreeVisualiser:
 
@@ -12,46 +13,64 @@ class TreeVisualiser:
 		self.Pos = {}
 		self.DepthNumNodes = {}
 		self.Labels = {}
+		self.FinishedNodes = []
+		self.NonFinishedNodes = []
+		self.MaxDepth = 12
+		self.EdgeCapPerNode = 3
 
-		depth = 12
-		self.Pos[startBoard] = [0,depth*100]
-		self.Labels[startBoard] = startBoard
-		self.DepthNumNodes[depth] = 1
-
-		self.BuildTree(startBoard, depth-1)
+		self.LastOutput = time.time()
+		self.BuildTree(startBoard)
 		self.DataSetManager.Clear()
 		self.ShowTree()
 		return
 	
-	def BuildTree(self, key, depth):
+	def BuildTree(self, key, depth=0):
+		if key in self.Labels:
+			return
+
+		if depth not in self.DepthNumNodes:
+			self.DepthNumNodes[depth] = 0
+
+		x = self.DepthNumNodes[depth]
+		if x % 2 == 0:
+			x *= -1
+		x /= 2
+		y = -depth*100
+
+		self.Pos[key] = [x,y]
+		self.DepthNumNodes[depth] += 1
+		self.Labels[key] = key
+
 		found, boardInfo = self.DataSetManager.GetBoardInfo(key)
 		if found:
-			for movekey, moveValue in boardInfo.Moves.items():
+			if boardInfo.Finished:
+				self.FinishedNodes += [key]
+			else:
+				self.NonFinishedNodes += [key]
 
-				for outComesKey, outComesValue in moveValue.MoveOutComes.items():
+			if time.time() - self.LastOutput >= 1:
+				self.ShowTree()
+				self.LastOutput = time.time()
 
-					self.Tree.add_edge(key, outComesKey)
-
-					
-					if outComesKey not in self.Pos:
-						if depth not in self.DepthNumNodes:
-							self.DepthNumNodes[depth] = 0
-
-						self.Pos[outComesKey] = [self.DepthNumNodes[depth], depth*100]
-						self.DepthNumNodes[depth] += 1
-						self.Labels[outComesKey] = outComesKey
-
-					if depth > 1:
-						self.BuildTree(outComesKey, depth-1)
+			if depth <= self.MaxDepth:
+				edges = 0
+				for movekey, moveValue in boardInfo.Moves.items():
+					for outComesKey, outComesValue in moveValue.MoveOutComes.items():
+						if edges < self.EdgeCapPerNode:
+							self.Tree.add_edge(key, outComesKey)
+							edges += 1
+						self.BuildTree(outComesKey, depth+1)
+		else:
+			self.FinishedNodes += [key]
 		return
 
 	def ShowTree(self):
 
-		plt.subplot(111)
-		#nx.draw_shell(self.Tree, with_labels=True, font_weight='bold')
+		#plt.subplot()
+		plt.plot()
 
-		nx.draw_networkx_nodes(self.Tree, self.Pos)
-
+		nx.draw_networkx_nodes(self.Tree, self.Pos, nodelist=self.NonFinishedNodes, node_color='r', node_size=100)
+		nx.draw_networkx_nodes(self.Tree, self.Pos, nodelist=self.FinishedNodes, node_color='g', node_size=100)
 		# edges
 		nx.draw_networkx_edges(self.Tree, self.Pos, alpha=0.5)
 
@@ -60,6 +79,6 @@ class TreeVisualiser:
 		print("Nodes: "+str(self.Tree.number_of_nodes()))
 		print("edges: "+str(self.Tree.number_of_edges()))
 		plt.axis('off')
-		plt.savefig("tree.svg", transparent=False)
+		#plt.savefig("tree.svg", transparent=False)
 		plt.show()
 		return
