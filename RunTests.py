d8eff20f45d233a009d946f7f6e79e5fdf4c75dd
louie jones-strong Laptop <louiej-s@hotmail.co.uk>
2019-07-07 17:06:39 +0100

    test for all evns

--- RunTests.py
+++ RunTests.py
@@ -1,4 +1,12 @@
 import RunController as runner
 
-controller = runner.RunController(simNumber=6, loadData="N", aiType="r", renderQuality=0, stopTime=60)
-controller.RunTournament()
+totalSimsNum = 6
+
+for simNum in range(1, totalSimsNum+1):
+
+	controller = runner.RunController(simNumber=simNum, loadData="N", aiType="b", renderQuality=0, stopTime=60)
+	controller.RunTournament()
+
+	controller = runner.RunController(simNumber=simNum, loadData="Y", aiType="b", renderQuality=0, stopTime=60)
+	controller.RunTournament()
+	
