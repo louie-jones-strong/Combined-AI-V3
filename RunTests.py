4d650032c53908ec9225b453657290c80697ff86
louie jones-strong Laptop <louiej-s@hotmail.co.uk>
2019-07-22 00:38:05 +0100

    output metadata files

--- RunTests.py
+++ RunTests.py
@@ -2,9 +2,12 @@ import RunController as runner
 from Shared import Logger
 import time
 from Shared.OutputFormating import SplitTime
+from DataManger import BasicLoadAndSave
 
 Sims = [1, 2, 3, 4, 6]
 Agents = ["b"]
+
+metaDataAddress = "TestOutputs//"
 hadError = False
 
 for simNum in Sims:
@@ -21,11 +24,18 @@ for simNum in Sims:
 
 			controller.RunTournament()
 			metaData1 = controller.AiDataManager.MetaData
+			
+			address = metaDataAddress+"MetaData_"+controller.SimInfo["SimName"]+"_"+agent+"_1"
+			BasicLoadAndSave.DictSave(address, metaData1)
 
 			controller = runner.RunController(simNumber=simNum, loadData="Y", aiType=agent, renderQuality=0, stopTime=10)
 			print("Run Done Took: "+SplitTime(time.time()-timeMarkRun, 2))
 			
 			metaData2 = controller.AiDataManager.MetaData
+
+			address = metaDataAddress+"MetaData_"+controller.SimInfo["SimName"]+"_"+agent+"_2"
+			BasicLoadAndSave.DictSave(address, metaData1)
+
 			if metaData1 != metaData2:
 				Logger.LogWarning("metaData1 != metaData2: save error?", holdOnInput=False)
 				hadError = True
