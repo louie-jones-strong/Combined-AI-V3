657f3c2024ab5e037d28dc34540ff5518fdf1722
louie jones-strong Laptop <louiej-s@hotmail.co.uk>
2019-07-22 00:11:42 +0100

    better test logs

--- RunTests.py
+++ RunTests.py
@@ -1,30 +1,45 @@
 import RunController as runner
 from Shared import Logger
+import time
+from Shared.OutputFormating import SplitTime
 
-sims = [1, 2, 3, 4, 6]
+Sims = [1, 2, 3, 4, 6]
+Agents = ["b"]
 hadError = False
 
-for simNum in sims:
+for simNum in Sims:
+	for agent in Agents:
+		print("Setup Sim: "+str(simNum)+" With Agent: "+str(agent))
+		input()
+		try:
+			timeMarkSetup = time.time()
+			controller = runner.RunController(simNumber=simNum, loadData="N", aiType=agent, renderQuality=0, stopTime=60)
 
-	try:
+			print("Setup Done Took: "+SplitTime(time.time()-timeMarkSetup, 2))
+			print("Sim = "+str(controller.SimInfo["SimName"]))
+			print("Starting Run...")
+			input()
+			timeMarkRun = time.time()
 
-		controller = runner.RunController(simNumber=simNum, loadData="N", aiType="b", renderQuality=0, stopTime=60)
-		controller.RunTournament()
-		metaData1 = controller.AiDataManager.MetaData["SizeOfDataSet"]
+			controller.RunTournament()
+			metaData1 = controller.AiDataManager.MetaData
 
-		controller = runner.RunController(simNumber=simNum, loadData="Y", aiType="b", renderQuality=0, stopTime=60)
-		
-		metaData2 = controller.AiDataManager.MetaData["SizeOfDataSet"]
-		if metaData1 != metaData2:
-			Logger.LogWarning("metaData1 != metaData2: save error?", holdOnInput=False)
+			controller = runner.RunController(simNumber=simNum, loadData="Y", aiType=agent, renderQuality=0, stopTime=10)
+			print("Run Done Took: "+SplitTime(time.time()-timeMarkRun, 2))
+			
+			metaData2 = controller.AiDataManager.MetaData
+			if metaData1 != metaData2:
+				Logger.LogWarning("metaData1 != metaData2: save error?", holdOnInput=False)
+				hadError = True
+			else:
+				controller.RunTournament()
+
+		except Exception as error:
 			hadError = True
+			Logger.LogError(error, holdOnInput=False)
 
-		controller.RunTournament()
 
-	except Exception as error:
-		hadError = True
-		Logger.LogError(error, holdOnInput=False)
-
+print("test Finished")
 if hadError:
 	exit(code=1)
 	
