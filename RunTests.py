c490cb29d90903e82876cbbcad91190a118cba94
louie jones-strong Laptop <louiej-s@hotmail.co.uk>
2019-07-31 21:36:54 +0100

    trying to add networking to tests

--- RunTests.py
+++ RunTests.py
@@ -14,14 +14,17 @@ class Tests:
 		self.Logger.LoadingBarAllowed = False
 
 		Sims = [1, 2, 3, 4, 6]
-		Agents = ["b"]
+		Agents = ["b","n"]
 
 		hadError = False
 
+		timeMark = time.time()
 		for simNum in Sims:
 			for agent in Agents:
 				if self.RunTest(simNum, agent):
 					hadError = True
+
+		print("Full Test Took: "+SplitTime(time.time()-timeMark))
 
 		print("tests Finished")
 
@@ -37,9 +40,9 @@ class Tests:
 
 		try:
 			timeMarkSetup = time.time()
-			controller = runner.RunController(self.Logger, simNumber=simNum, loadData="N", aiType=agent, renderQuality=0, stopTime=60)
+			controller = runner.RunController(self.Logger, simNumber=simNum, loadData="Y", aiType=agent, renderQuality=0, trainNetwork="Y", stopTime=60)
 
-			print("Setup Done Took: "+SplitTime(time.time()-timeMarkSetup, 2))
+			print("Setup Done Took: "+SplitTime(time.time()-timeMarkSetup))
 			print("Sim = "+controller.SimInfo["SimName"])
 			print("Starting Run...")
 			timeMarkRun = time.time()
@@ -47,16 +50,15 @@ class Tests:
 			controller.RunTournament()
 			metaData1 = controller.AiDataManager.MetaData
 
-			address = self.MetaDataAddress+"MetaData_" + \
-				controller.SimInfo["SimName"]+"_"+agent+"_1"
+			address = self.MetaDataAddress+"MetaData_" +controller.SimInfo["SimName"]+"_"+agent+"_1"
 			BasicLoadAndSave.DictSave(address, metaData1)
 
-			controller = runner.RunController(self.Logger, simNumber=simNum, loadData="Y", aiType=agent, renderQuality=0, stopTime=10)
-			print("Run Done Took: "+SplitTime(time.time()-timeMarkRun, 2))
+			print("Run+Save Took: "+SplitTime(time.time()-timeMarkRun))
+
+			controller = runner.RunController(self.Logger, simNumber=simNum, loadData="Y", aiType=agent, renderQuality=0, trainNetwork="N", stopTime=10)
 
 			metaData2 = controller.AiDataManager.MetaData
-			address = self.MetaDataAddress+"MetaData_" + \
-				controller.SimInfo["SimName"]+"_"+agent+"_2"
+			address = self.MetaDataAddress+"MetaData_" + controller.SimInfo["SimName"]+"_"+agent+"_2"
 			BasicLoadAndSave.DictSave(address, metaData2)
 
 			metaDataSame = True
@@ -66,8 +68,7 @@ class Tests:
 					break
 
 			if not metaDataSame:
-				self.Logger.LogWarning("sim: "+controller.SimInfo["SimName"]+" AI: " +
-									agent+" metaData1 != metaData2: save error?", holdOnInput=False)
+				self.Logger.LogWarning("sim: "+controller.SimInfo["SimName"]+" AI: " +agent+" metaData1 != metaData2: save error?", holdOnInput=False)
 				hadError = True
 			else:
 				controller.RunTournament()
