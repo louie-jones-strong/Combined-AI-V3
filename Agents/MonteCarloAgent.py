03de415b12b7253d16887ecfc48cff6ff05bb150
louie jones-strong Laptop <louiej-s@hotmail.co.uk>
2019-09-08 23:38:12 +0100

    next part of monte carlo agent

--- Agents/MonteCarloAgent.py
+++ Agents/MonteCarloAgent.py
@@ -1,5 +1,6 @@
 import Agents.AgentBase as AgentBase
 import Agents.SimOutputPredictor as BoardPredictor
+import sys
 
 class Agent(AgentBase.AgentBase):
 
@@ -10,7 +11,9 @@ class Agent(AgentBase.AgentBase):
 		self.MoveAgent.RecordMoves = False
 
 		self.BoardPredictor = BoardPredictor.SimOutputPredictor(dataSetManager, loadData)
-		self.Depth = 3
+
+		self.MaxMoveCalDepth = 3
+		self.MaxMoveCalTime = 1
 		return
 
 	def MoveCal(self, board):
@@ -19,3 +22,38 @@ class Agent(AgentBase.AgentBase):
 		self.RecordMove(board, move)
 		return move
 
+	def AlphaBeta(self, board, alpha=-sys.maxsize, beta=sys.maxsize, isMax=True):
+		minv = sys.maxsize
+		maxv = -sys.maxsize
+		bestMove = None
+
+		moveList = self.MoveAgent.MoveCal(board)
+
+		for tempMove in moveList:
+			
+			predictedBoard = self.BoardPredictor.PredictOutput(board, tempMove)
+
+			if predictedBoard != "Finished":
+				m, tempMove = self.AlphaBeta(predictedBoard, alpha=alpha, beta=beta, isMax=not isMax)
+
+
+				if isMax:
+					if m > maxv:
+						maxv = m
+						bestMove = tempMove
+
+						if maxv >= beta:
+							return maxv, bestMove
+						elif maxv > alpha:
+							alpha = maxv
+				else:
+					if m < minv:
+						minv = m
+
+						if minv <= alpha:
+							return minv, bestMove
+
+						elif minv < beta:
+							beta = minv
+
+		return minv, bestMove
