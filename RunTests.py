82aa72d42020c5eea04eb1297a53e42e17e2d47c
Louie <louiej-s@hotmail.co.uk>
2019-09-25 19:41:32 +0100

    revert

--- RunTests.py
+++ RunTests.py
@@ -3,7 +3,6 @@ from Shared import Logger
 import time
 from Shared.OutputFormating import SplitTime, TimeToDateTime
 from DataManger import BasicLoadAndSave
-import test
 
 class Tests:
 	def __init__(self):
@@ -24,10 +23,6 @@ class Tests:
 		Agents = ["b", "n", "e"]
 
 		hadError = False
-		
-		test.TensorflowTest()
-		test.TfLearnTest()
-
 		timeMark = time.time()
 		for simNum in Sims:
 			for agent in Agents:
